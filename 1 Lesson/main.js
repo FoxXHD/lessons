d03612b057c8e8668747312b555baeefb2a7eeec
FoxXHD <kirakosanmaksim51@gmail.com>
2022-08-11 23:02:52 +0300

    lesson 2

--- 1 Lesson/main.js
+++ 1 Lesson/main.js
@@ -1,3 +1,41 @@
 console.log("arknights")
 console.log("0")
 console.log("4+8")
+
+//lesson 2
+
+// 1 exercise
+
+console.log(11)
+console.log('november')
+
+// 2 exercise
+
+console.log("добро " + "пожаловать " + "на " + "курс, " + "Максим Киракосян!" )
+
+// 3 exercise
+
+alert("11.08.2022")
+
+// 4 exercise
+
+document.getElementById("one").innerHTML = "hello world"
+
+// 5 exercise 
+
+let num = 21
+console.log(num)
+
+let text ="Terra"
+console.log(text)
+
+let Bigi = 124967n
+console.log(Bigi)
+
+let Text2 = null
+console.log(Text2)
+
+console.log(undefined)
+
+let Check = "true"
+console.log(Check)
