48389f9cde5919cdfab55b3b8eb7b1adf0c88306
FoxXHD <kirakosanmaksim51@gmail.com>
2022-09-01 17:38:51 +0300

    lession 5

---
+++ lession 5/main.js
@@ -0,0 +1,66 @@
+ // Циклы
+ // while sintax
+
+/* while (условие) {
+
+
+} */
+
+ /*  while (conditon) {
+
+ } */
+
+ /* let a = 0
+ while (a < 5) {
+    alert (a)
+    a++  // a = a + 1
+ } */
+
+ /* let a = 5
+ while (a > 1) {
+    alert (a)
+    a--  // a = a - 1
+ }
+ */
+ //++ - увеличить на 1
+ //-- - уменьшить на 1 
+
+ // do..while - доп консрукт
+ 
+/* do {
+    // тело/body цикла
+ }while (условие) */
+
+ // for - цикл
+
+/* for (начало;условие;шаг) {
+    //тело/body цикла
+} */
+
+/* for (let a = 0;a < 5;a++) {
+    alert (a)
+} */
+
+/* let a = 0
+for (a;a < 10;a++) {
+    if (a % 2 == 0) continue
+} */
+
+/* for (let a = 0;0 < 100; ) {
+    if (a % 2 == 0) continue
+    console.log(a)
+} */
+
+// dz (1)
+
+
+for(let a = 2;a <= 10;a++) {
+    if (a % 2 == 0) console.log(a)
+    continue
+}
+
+// dz (2)
+
+ for(let b = 0;b<=100;b++) {
+    console.log(b)
+}
